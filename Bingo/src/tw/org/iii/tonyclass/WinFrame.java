4f1ae58bc310a675d6af292a0426ac401f1586f7
User <User@WS7.r1802.tciii.org.tw>
2023-07-10 10:14:03 +0800

    07101014

--- Bingo/src/tw/org/iii/tonyclass/WinFrame.java
+++ Bingo/src/tw/org/iii/tonyclass/WinFrame.java
@@ -3,58 +3,44 @@ package tw.org.iii.tonyclass;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-@SuppressWarnings("serial")
-public class WinFrame extends JFrame {
-	private Map<String, List<JButton>> buttons = new HashMap<String,List<JButton>>();
+public class WinFrame extends JFrame{
+
+
+private Map<String, List<JButton>> buttons = new HashMap<String,List<JButton>>();
 	
 	private JTextField jtfRoom,jtfName,jtfIP;
-	private JTextArea jtaR,jtaD;
-	private JScrollPane jspR,jspD;
-	private JButton[] bingo;
-	private JButton connectIP,start,auto,clear,exit;
-	private boolean room=false,startF=false;
-//			,ready=false,yourturn=false,roomReady=false;
-//	private int[] nums;
-//	private int num = 0;
-//	private int width = -1;
-//	private int height = -1;
-//	private String roomID = "";
-	
-	
-//	public  ClientFrame() {
-//		regElement();
-//		initComp();
-//	}
+	private static JTextArea jtaR;
+//	private JTextArea jtaD;
+	private JScrollPane jspR;
+//	jspD;
+	private static JButton[] bingo;
+	private JButton connectIP;
 
-//  有menu版本
-//	private void addButton(String key,JButton button,boolean flag) {
-//		if(! buttons.containsKey(key)) {
-//			buttons.put(key, new ArrayList<JButton>());
-//		}
-//		
-//		List<JButton> buttons = this.buttons.get(key);
-//		if(!buttons.contains(button)) {
-//			buttons.add(button);
-//			if(flag) {
-//				add(button);
-//			}
-//		}
-//		
-//	}
+	private static JButton start;
+
+	private static JButton auto;
+
+	private static JButton clear;
+
+	private JButton exit;
+//	private boolean room=false,startF=false;
+	private static int num = 1, rdn ,index = 0;
+	static int[] Number = new int[25];
+
 	
 	private void addButton(String key,JButton button) {
 		if(! buttons.containsKey(key)) {
@@ -93,9 +79,9 @@ public class WinFrame extends JFrame {
 		jtfName = new JTextField();
 		jtfIP = new JTextField();
 		jtaR = new JTextArea();
-		jtaD = new JTextArea();
-//		jspR = new JScrollPane();
-//		jspD = new JScrollPane();
+//		jtaD = new JTextArea();
+		jspR = new JScrollPane(jtaR);
+//		jspD = new JScrollPane(jtaD);
 		connectIP = new JButton();
 		start = new JButton("開始");
 		auto = new JButton("自動選號");
@@ -120,33 +106,30 @@ public class WinFrame extends JFrame {
         connectIP.setText("連接");
         add(connectIP);
         
-//        jspR.setBounds(550, 60, 140, 330);
-//        add(jspR);
-//        
-//        jspD.setBounds(550,410,140,80);
-//        add(jspD);
+        jspR.setBounds(550, 60, 140, 420);
+        jtaR.setEditable(false);
+        add(jspR);
         
+//        jspD.setBounds(550, 410, 140, 80);
+//        add(jspD);
         
         start.setBounds(30,360,240,60);
         addButton("start", start);
         
-//        addButton("menu", start,false);
+
         
         auto.setBounds(280,360,240,60);
+        auto.setEnabled(false);
         addButton("auto", auto);
-//        addButton("menu", auto,false);
+
         
         clear.setBounds(30,430,240,60);
+        clear.setEnabled(false);
         addButton("clear", clear);
-//        addButton("menu", clear,false);
-		
+
         exit.setBounds(280,430,240,60);
         addButton("exit", exit);
-//        addButton("menu", exit,false);
-        
-//        for (JButton menu : this.getButton("menu")) {
-//            menu.setEnabled(false);
-//        }
+
         
         SwingUtilities.updateComponentTreeUI(this);
         
@@ -156,22 +139,29 @@ public class WinFrame extends JFrame {
 	
 	//遊戲運行初始化
 	private void initComp() {
-		connectIP.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 		
 		start.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-//				System.out.println(e);
+				start.setEnabled(false);
+				auto.setEnabled(true);
+				clear.setEnabled(true);
+				if(start.getText().equals("準備")|| num == 26) {
+					for(JButton start : getButton("start")) {
+						start.setEnabled(false);
+					}
+					for(JButton auto : getButton("auto")) {
+						auto.setEnabled(false);
+					}
+					for(JButton clear : getButton("clear")) {
+						clear.setEnabled(false);
+					}
+				}
+				
 				start();
-				checkProgress();
+				
+				
 				
 			}
 		});
@@ -180,8 +170,9 @@ public class WinFrame extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				clear();
 				auto();
-				checkProgress();
+				start.setEnabled(true);
 				
 			}
 		});
@@ -191,7 +182,7 @@ public class WinFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				clear();
-				checkProgress();
+				start.setEnabled(false);
 				
 			}
 		});
@@ -206,105 +197,252 @@ public class WinFrame extends JFrame {
 			}
 		});
 		
-		jtaD.addKeyListener(new KeyListener() {
-			
-			boolean flagS = false;
-			boolean flagE = false;
-			
-			
-			@Override
-			//按著不做功能
-			public void keyTyped(KeyEvent e) {
-				
-			}
-			
-			@Override
-			//釋放按鈕 未完成
-			public void keyReleased(KeyEvent e) {
-				if (flagE) { //按下enter
-					if(flagS) { //強迫換行
-						jtaD.setText(jtaD.getText()+"\r\n");
-					}else { //送出
-//						send
-					}
-				}
-//				if ()
-				
-			}
-			
-			@Override
-			//按下按鈕
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode()==KeyEvent.VK_ENTER) {
-					flagE = true;
-				}
-				if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
-					flagS = true;
-				}
-			}
-		});
+//		jtaD.addKeyListener(new KeyListener() {
+//			
+//			boolean flagS = false;
+//			boolean flagE = false;
+//			
+//			
+//			@Override
+//			//按著不做功能
+//			public void keyTyped(KeyEvent e) {
+//				
+//			}
+//			
+//			@Override
+//			//釋放按鈕 未完成
+//			public void keyReleased(KeyEvent e) {
+//				if (flagE) { //按下enter
+//					if(flagS) { //強迫換行
+//						jtaD.setText(jtaD.getText()+"\r\n");
+//					}else { //送出
+////						send
+//					}
+//				}
+////				if ()
+//				
+//			}
+//			
+//			@Override
+//			//按下按鈕
+//			public void keyPressed(KeyEvent e) {
+//				if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+//					flagE = true;
+//				}
+//				if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+//					flagS = true;
+//				}
+//			}
+//		});
 		
 		
 	}
 	
 	
 	
-//	private void sendChat(String msg) {
-//		
+	
+	
+//	private void checkProgress() {
+//		if(room) {
+//			if (startF) {
+//				for (JButton menu : this.getButton("menu")) {
+//					menu.setEnabled(false);
+//				}
+//			}else {
+//				for(JButton menu : this.getButton("munu")) {
+//					menu.setEnabled(true);
+//				}
+//			}
+//			
+//		}
 //	}
-	
-	
-	
-	
-	private void checkProgress() {
-		if(room) {
-			if (startF) {
-				for (JButton menu : this.getButton("menu")) {
-					menu.setEnabled(false);
-				}
-			}else {
-				for(JButton menu : this.getButton("munu")) {
-					menu.setEnabled(true);
-				}
-			}
-			
-		}
-	}
 	
 	
 	//各按鈕功能
 	private void start() {
-		for(JButton start : getButton("start")) {
-			start.setText("準備");
-		}
 		
-//		System.out.println("text");
-		int w = 450/5;
-		int h = 250/5;
-		this.bingo = new JButton[5*5];
-		for (int x=0 ; x<5;x++) {
-			for(int y=0;y<5;y++) {
-				JButton bingo = new JButton();
-				bingo.setBounds(30+x*(w+10), 60+y*(h+10), w, h);
-				addButton("bingo", bingo);
-				this.bingo[y*5+x] = bingo;
-
-				bingo.addActionListener(new ActionListener() {
-
-					public void actionPerformed(ActionEvent e) {
-						WinFrame.this.bingo[10].setBackground(Color.pink);
-						WinFrame.this.bingo[10].setText(Integer.toString(1));
-						
+		if(start.getText().equals("準備")) {
+			boolean isRepeat;
+			for(int i=0 ; i<25;i++) {
+				do {
+					
+					rdn = (int)(Math.random()*25+1);
+					//檢查機制
+					isRepeat = false;
+					for (int j=0; j<i; j++) {
+						if (Number[j] == rdn) {
+							isRepeat = true;
+							break;
+						}
 					}
-				});
+					
+				}while(isRepeat);
+				Number[i] = rdn;
 			}
+			
+			index = 1;
+			jtaR();
+			
+			
+		}else if(start.getText().equals("開始")) {
+			for(JButton start : getButton("start")) {
+				start.setText("準備");
+			}
+			int w = 450/5;
+			int h = 250/5;
+			this.bingo = new JButton[5*5];
+			for (int x=0 ; x<5;x++) {
+				for(int y=0;y<5;y++) {
+					JButton bingo = new JButton();
+					bingo.setBounds(30+x*(w+10), 60+y*(h+10), w, h);
+					addButton("bingo", bingo);
+					this.bingo[y*5+x] = bingo;
+
+					bingo.addActionListener(new NumberbuttonAction(this, x, y) {
+					});
+					
+				}
+			}
+			repaint(); //渲染
 		}
-		repaint();
-		
-		
-		
 	}
 	
+	private static class NumberbuttonAction  implements ActionListener {
+		
+		final int pos;
+		final WinFrame core;
+		
+		private NumberbuttonAction (WinFrame core,int x ,int y) {
+			this.core = core;
+			this.pos = y * 5 + x;
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			if(bingo[pos].getBackground() != Color.pink
+					&& bingo[pos].getBackground() != Color.red ) {
+				bingo[pos].setBackground(Color.pink);
+				bingo[pos].setText(Integer.toString(num++));
+			}
+			if (num == 26 && auto.isEnabled()) {
+				start.setEnabled(true);
+				
+			}
+			play(pos);
+
+			
+			
+
+		}
+	}
+	
+	private static void play(int pos) {
+		
+		
+		
+		int line = 0;
+		if (!start.isEnabled() && !auto.isEnabled() && !clear.isEnabled()) {
+			if(bingo[pos].getText().equals(Integer.toString(Number[index]) )) {
+				bingo[pos].setBackground(Color.red);
+				index ++;
+				jtaR.append("\n第"+index+"個數字"+Integer.toString(Number[index]));
+			}else {
+				jtaR.append("\n選錯囉，請選"+Number[index]);
+			}
+			
+		}
+		if (bingo[0].getBackground()== Color.red &&
+			bingo[1].getBackground()== Color.red &&
+			bingo[2].getBackground()== Color.red &&
+			bingo[3].getBackground()== Color.red &&
+			bingo[4].getBackground()== Color.red ) {
+			line++;
+		}
+		if (bingo[5].getBackground()== Color.red &&
+			bingo[6].getBackground()== Color.red &&
+			bingo[7].getBackground()== Color.red &&
+			bingo[8].getBackground()== Color.red &&
+			bingo[9].getBackground()== Color.red ) {
+				line++;
+			}
+		if (bingo[10].getBackground()== Color.red &&
+			bingo[11].getBackground()== Color.red &&
+			bingo[12].getBackground()== Color.red &&
+			bingo[13].getBackground()== Color.red &&
+			bingo[14].getBackground()== Color.red ) {
+				line++;
+			}
+		if (bingo[15].getBackground()== Color.red &&
+			bingo[16].getBackground()== Color.red &&
+			bingo[17].getBackground()== Color.red &&
+			bingo[18].getBackground()== Color.red &&
+			bingo[19].getBackground()== Color.red ) {
+					line++;
+				}
+		if (bingo[20].getBackground()== Color.red &&
+			bingo[21].getBackground()== Color.red &&
+			bingo[22].getBackground()== Color.red &&
+			bingo[23].getBackground()== Color.red &&
+			bingo[24].getBackground()== Color.red ) {
+					line++;
+				}
+		if (bingo[0].getBackground()== Color.red &&
+			bingo[5].getBackground()== Color.red &&
+			bingo[10].getBackground()== Color.red &&
+			bingo[15].getBackground()== Color.red &&
+			bingo[20].getBackground()== Color.red ) {
+					line++;
+				}
+		if (bingo[1].getBackground()== Color.red &&
+			bingo[6].getBackground()== Color.red &&
+			bingo[11].getBackground()== Color.red &&
+			bingo[16].getBackground()== Color.red &&
+			bingo[21].getBackground()== Color.red ) {
+					line++;
+				}
+		if (bingo[2].getBackground()== Color.red &&
+			bingo[7].getBackground()== Color.red &&
+			bingo[12].getBackground()== Color.red &&
+			bingo[17].getBackground()== Color.red &&
+			bingo[22].getBackground()== Color.red ) {
+					line++;
+				}
+		if (bingo[3].getBackground()== Color.red &&
+			bingo[8].getBackground()== Color.red &&
+			bingo[13].getBackground()== Color.red &&
+			bingo[18].getBackground()== Color.red &&
+			bingo[23].getBackground()== Color.red ) {
+					line++;
+				}
+		if (bingo[4].getBackground()== Color.red &&
+			bingo[9].getBackground()== Color.red &&
+			bingo[14].getBackground()== Color.red &&
+			bingo[19].getBackground()== Color.red &&
+			bingo[24].getBackground()== Color.red ) {
+					line++;
+				}
+		if (bingo[0].getBackground()== Color.red &&
+			bingo[6].getBackground()== Color.red &&
+			bingo[12].getBackground()== Color.red &&
+			bingo[18].getBackground()== Color.red &&
+			bingo[24].getBackground()== Color.red ) {
+					line++;
+				}
+		if (bingo[4].getBackground()== Color.red &&
+			bingo[8].getBackground()== Color.red &&
+			bingo[12].getBackground()== Color.red &&
+			bingo[16].getBackground()== Color.red &&
+			bingo[20].getBackground()== Color.red ) {
+					line++;
+				}
+		ImageIcon picture = new ImageIcon("src/tw/org/iii/tonyclass/bingo.png");
+		
+		
+		if (line == 3) {
+			JOptionPane.showConfirmDialog(null, "", "BinGo", JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE,picture);
+		}
+		
+
+	}
 	
 	
 	private void auto() {
@@ -328,19 +466,20 @@ public class WinFrame extends JFrame {
 			}while(isRepeat);
 			ButtonNumber[i] = n;
 			
-//			System.out.println(n);
-//			System.out.println(bingo[n]);
+
 			if (bingo[n].getBackground() != Color.pink) {
 				bingo[n].setText(Integer.toString(i+1));
 	            bingo[n].setBackground(Color.pink);
 			}
 			
 		}
-		
+
 		
 	}
 	
 	private void clear() {
+		
+		num = 1;
 		for (JButton bingo : getButton("bingo")) {
             bingo.setText("");
             bingo.setBackground(null);
@@ -350,13 +489,16 @@ public class WinFrame extends JFrame {
 	private void exit() {
 		System.exit(0);
 	}
+	
+	private static void jtaR() {
+		
+		jtaR.setText("第"+index+"個數字"+Integer.toString(Number[index]) );
+		
+		
+	}
 
 	
-	
-	
-	
-	
-	
+
 	
 	public static void main(String[] args) {
 		WinFrame fwk =	new WinFrame();
@@ -365,5 +507,7 @@ public class WinFrame extends JFrame {
 		
 		
 	}
+
+	
 
 }
